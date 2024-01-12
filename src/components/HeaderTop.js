c1920ed5c271c80e2c0ec0592c13cf19a0054e62
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-12 13:37:34 +0530

    :recycle: refactor: Moves status bar to screen component

--- src/components/HeaderTop.js
+++ src/components/HeaderTop.js
@@ -1,19 +1,17 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 
 import HeaderTitle from "./HeaderTitle";
 import SearchBar from "./SearchBar";
 import DeliveryDropdown from "./DeliveryDropdown";
-import { StatusBar } from "expo-status-bar";
 
 export default function HeaderTop() {
   return (
-    <SafeAreaView style={styles.headerTop}>
-      <StatusBar style="light" />
+    <View style={styles.headerTop}>
       <HeaderTitle />
       <SearchBar />
       <DeliveryDropdown />
-    </SafeAreaView>
+    </View>
   );
 }
 const styles = StyleSheet.create({
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
-import { StyleSheet, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import React from "react";
 import HeaderTop from "../components/HeaderTop";
+import { StatusBar } from "expo-status-bar";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
+      <StatusBar style="light" />
       <HeaderTop />
     </SafeAreaView>
   );
