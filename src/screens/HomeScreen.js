aef91b08f7f9872bee9f0afa81788c61ddc7fd37
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-12 14:13:48 +0530

    :sparkles: feat: Completes offers carousel component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,14 @@ import { SafeAreaView } from "react-native";
 import React from "react";
 import HeaderTop from "../components/HeaderTop";
 import { StatusBar } from "expo-status-bar";
+import OffersCarousel from "../components/OffersCarousel";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <StatusBar style="light" />
       <HeaderTop />
+      <OffersCarousel />
     </SafeAreaView>
   );
 }
