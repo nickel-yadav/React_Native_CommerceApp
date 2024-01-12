f4836e9ae1208788ea9d7ec1db12604c14e37f30
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-12 15:42:27 +0530

    :recycle: refactor: Moves statusbar to outermost parent

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View } from "react-native";
 import React from "react";
 import HeaderTop from "../components/HeaderTop";
 import { StatusBar } from "expo-status-bar";
@@ -7,11 +7,13 @@ import ProductList from "../components/ProductList";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <View>
       <StatusBar style="light" />
-      <HeaderTop />
-      <OffersCarousel />
-      <ProductList />
-    </SafeAreaView>
+      <SafeAreaView>
+        <HeaderTop />
+        <OffersCarousel />
+        <ProductList />
+      </SafeAreaView>
+    </View>
   );
 }
