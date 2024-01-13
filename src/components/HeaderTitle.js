c6e0d0b2e977c7ca292be9ae312e42a8042dfa2a
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-13 20:22:38 +0530

    :recycle: refactor: Improved badge counter and adds nav to cart

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -2,17 +2,29 @@ import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HeaderTitle({ userName }) {
+  const navigation = useNavigation();
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const itemCount = cartItems.length;
+
+  const handleNavToCart = () => {
+    navigation.navigate("Cart");
+  };
+
+  const getTotalQuantity = () => {
+    return cartItems.reduce((totalQuantity, item) => {
+      return totalQuantity + (item.itemQuantity || 0);
+    }, 0);
+  };
+
   return (
     <View style={styles.cartContainer}>
       <Text style={styles.heading}>Hey {userName}</Text>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleNavToCart}>
         <SimpleLineIcons name="handbag" size={16} color="white" />
         <View style={styles.badge}>
-          <Text style={styles.badgeText}>{itemCount}</Text>
+          <Text style={styles.badgeText}>{getTotalQuantity()}</Text>
         </View>
       </TouchableOpacity>
     </View>
