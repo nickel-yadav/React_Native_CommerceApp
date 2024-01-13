1045993de12c2a8b5583204143138bdc10ee87f6
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-13 20:12:57 +0530

    :lipstick: feat: Adds QOL item counter badge to shopping bag

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -1,13 +1,19 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function HeaderTitle({ userName }) {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const itemCount = cartItems.length;
   return (
     <View style={styles.cartContainer}>
       <Text style={styles.heading}>Hey {userName}</Text>
       <TouchableOpacity>
         <SimpleLineIcons name="handbag" size={16} color="white" />
+        <View style={styles.badge}>
+          <Text style={styles.badgeText}>{itemCount}</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -24,5 +30,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     marginTop: 52,
+  },
+  badge: {
+    position: "absolute",
+    bottom: 12,
+    left: 8,
+    backgroundColor: "#FFC83A",
+    paddingHorizontal: 4,
+    borderRadius: 100,
+  },
+  badgeText: {
+    fontSize: 12,
+    fontFamily: "manrope-800",
   },
 });
