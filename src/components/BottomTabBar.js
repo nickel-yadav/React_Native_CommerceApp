91344ecbced464a54dcec32f5dc955834581e5f5
Nikhil Yadav <nickelyadav57@gmail.com>
2024-01-14 11:36:42 +0530

    :lipstick: feat: Improves tab bar styling and adds icon

--- src/components/BottomTabBar.js
+++ src/components/BottomTabBar.js
@@ -1,8 +1,11 @@
 import { View, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 export default function BottomTabBar({ state, descriptors, navigation }) {
   return (
-    <View style={{ flexDirection: "row" }}>
+    <View
+      style={{ flexDirection: "row", padding: 10, backgroundColor: "white" }}
+    >
       {state.routes.map((route, index) => {
         const { options } = descriptors[route.key];
         const label =
@@ -27,8 +30,23 @@ export default function BottomTabBar({ state, descriptors, navigation }) {
         };
 
         return (
-          <TouchableOpacity key={index} onPress={onPress} style={{ flex: 1 }}>
-            <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
+          <TouchableOpacity
+            key={index}
+            onPress={onPress}
+            style={{ flex: 1, alignItems: "center" }}
+          >
+            <Feather
+              name="home"
+              size={16}
+              color={isFocused ? "#2A4BA0" : "#8891A5"}
+            />
+            <Text
+              style={{
+                color: isFocused ? "#2A4BA0" : "#8891A5",
+                fontSize: 12,
+                fontFamily: "manrope-500",
+              }}
+            >
               {label}
             </Text>
           </TouchableOpacity>
